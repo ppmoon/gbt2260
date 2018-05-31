8e47740b570802643a49899992a0e2b7a5479dcf
ppmoon <272305610@qq.com>
2018-05-31 15:47:19 +0800

    更新README.MD使用说明 修复一个golang相对路径的bug

--- gbt2260.go
+++ gbt2260.go
@@ -1,18 +1,22 @@
 package gbt2260
 
 import (
-	"os"
-	"fmt"
 	"encoding/csv"
+	"fmt"
 	"io"
+	"os"
 )
-type BGT2260 struct {}
+
+type BGT2260 struct{}
+
 //通过全局的方式创建trie树
 var trie = New()
 
 func NewGBT2260() *BGT2260 {
 	//读取文件
-	file,err := os.Open("GBT2260-201802.csv")
+	//获取相对路径
+	pwd,_ := os.Getwd()
+	file, err := os.Open(pwd+"/GBT2260-201802.csv")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
@@ -27,12 +31,13 @@ func NewGBT2260() *BGT2260 {
 		}
 		code := record[0]
 		name := record[1]
-		createTrieTree(code,name,trie)
+		createTrieTree(code, name, trie)
 	}
 	return &BGT2260{}
 }
+
 //向树中插入数据
-func createTrieTree(code string,name string,trie *Trie) {
+func createTrieTree(code string, name string, trie *Trie) {
 	//检查传递参数
 	if code == "" || len(code) == 0 {
 		return
@@ -41,27 +46,29 @@ func createTrieTree(code string,name string,trie *Trie) {
 	var lCode = stringParse(code)
 	//创建trie树
 	trieRoot := trie
-	trieRoot.Add(lCode,name)
+	trieRoot.Add(lCode, name)
 }
+
 //将传入的字符串解析成字符串数组
-func stringParse (str string) []string{
+func stringParse(str string) []string {
 	var lCode []string
-	for i := 0;i<len(str)/2 ;i++  {
+	for i := 0; i < len(str)/2; i++ {
 		if str[2*i:2*(i+1)] != "00" {
-			lCode = append(lCode,str[2*i:2*(i+1)] )
+			lCode = append(lCode, str[2*i:2*(i+1)])
 		}
 	}
 	return lCode
 }
+
 //从树里面读取数据
-func (b *BGT2260) SearchGBT2260 (code string) []string {
+func (b *BGT2260) SearchGBT2260(code string) []string {
 	var lCode = stringParse(code)
 	var newCode = []string{}
 	node := trie.Root()
-	for i := range lCode{
+	for i := range lCode {
 		r := lCode[i]
-		if n,ok := node.children[r]; ok {
-			newCode = append(newCode,n.value)
+		if n, ok := node.children[r]; ok {
+			newCode = append(newCode, n.value)
 			node = n
 		} else {
 			fmt.Printf("对不起，您输入的地域码不在列表当中")
