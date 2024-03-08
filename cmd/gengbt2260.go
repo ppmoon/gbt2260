ead2de8a74f82bbf09556a7ea06b6234e1af7d8a
cuiyuanrong <cyr520@hotmail.com>
2024-03-08 20:56:07 +0800

    修复以北京市为省份查询出来的城市列表异常问题

--- cmd/gengbt2260.go
+++ cmd/gengbt2260.go
@@ -30,6 +30,8 @@ func CreateGBT2260Table() {
 		name := record[1]
 		line = line + "{\"" + code + "\",\"" + name + "\"},"
 	}
+	// 官方提供数据有缺失，手工补充直辖市，市辖区编码
+	line = line + "{\"110100\",\"市辖区\"},{\"120100\",\"市辖区\"},{\"310100\",\"市辖区\"},{\"500100\",\"市辖区\"},"
 	content := "package gbt2260;func GetGbt2260Table() [][]string {gbt2260Table := [][]string{" + line + "};return gbt2260Table;}"
 	gbtFile, err := os.Create("gbt2260Table.go")
 	if err != nil {
